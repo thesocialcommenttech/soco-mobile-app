2163067c3e29d9d3f6111179d5b7bc9fb0da9a61
Kapadiatathya <kapadiatathya@gmail.com>
2022-05-28 15:38:05 +0530

    KYC screen in wallet

---
+++ src/screens/Setting/wallet/wallet.tsx
@@ -0,0 +1,9 @@
+//import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Kyc from './kyc';
+
+export default function Wallet() {
+  return <Kyc />;
+}
+
+//const styles = StyleSheet.create({});
