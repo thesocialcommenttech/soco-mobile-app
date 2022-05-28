22af12f6abd71abab666ee7311b53f3362503437
Kapadiatathya <kapadiatathya@gmail.com>
2022-05-28 15:35:33 +0530

    navigation from stack navigation for settings

--- src/navigation/mainStack.tsx
+++ src/navigation/mainStack.tsx
@@ -2,14 +2,12 @@ import * as React from 'react';
 
 import HomeScreen from '../screens/home/home';
 import ProfileScreen from '../screens/profile/profile';
-import SettingStack from './settingStack';
 
 export default function (Stack) {
   return (
     <>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="Settings" component={SettingStack} />
     </>
   );
 }
--- src/navigation/settingStack.tsx
+++ src/navigation/settingStack.tsx
@@ -1,40 +1,38 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import mainSettingScreen from '../screens/Setting/mainSettingScreen/mainSettingScreen';
-import InterestScreen from '../screens/Setting/interest/interest';
-import Subscriptions from '../screens/Setting/subsciptions/subscriptions';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import Interest from '../screens/Setting/interest/interest';
 import Notification from '../screens/Setting/notification/notification';
-import Referals from '../screens/Setting/referals/referals';
+import Main from '../screens/Setting/SettingScreen/SettingScreen';
+import Referral from '../screens/Setting/referals/referals';
+import Subscriptions from '../screens/Setting/subsciptions/subscriptions';
+import Profile from '../screens/Setting/profile/profile';
+import Password from '../screens/Setting/password/password';
+import Wallet from '../screens/Setting/wallet/wallet';
 
-const Tab = createMaterialTopTabNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function settingStack() {
+export default function originalSettingStack() {
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: 'white'
+    }
+  };
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarScrollEnabled: true,
-          tabBarIndicatorStyle: {
-            backgroundColor: 'blue',
-            height: 3
-          }
-        }}
-        sceneContainerStyle={styles.container}
-      >
-        <Tab.Screen name="Home" component={mainSettingScreen} />
-        <Tab.Screen name="Interest" component={InterestScreen} />
-        <Tab.Screen name="Subscription" component={Subscriptions} />
-        <Tab.Screen name="Notification" component={Notification} />
-        <Tab.Screen name="Referral" component={Referals} />
-      </Tab.Navigator>
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator initialRouteName="Setting">
+        <Stack.Screen name="Setting" component={Main} />
+        <Stack.Screen name="Interests" component={Interest} />
+        <Stack.Screen name="Notification" component={Notification} />
+        <Stack.Screen name="Referral" component={Referral} />
+        <Stack.Screen name="Subscription" component={Subscriptions} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Password" component={Password} />
+        <Stack.Screen name="Wallet" component={Wallet} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white'
-  }
-});
