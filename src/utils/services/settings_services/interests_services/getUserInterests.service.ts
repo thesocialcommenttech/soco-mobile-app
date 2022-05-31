d3c5815a5180bc14ea584af071dfa3282c95bd1c
Pratik1803 <pratikvaidya1803@gmail.com>
2022-05-31 11:42:45 +0530

    changeNotificationSettings, getInterestsCategories, getUserInterests services added

---
+++ src/utils/services/settings_services/interests_services/getUserInterests.service.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { GetUserInterestsResponse } from '~/src/utils/typings/settings_interfaces/interests_interface/getUserInterests.interface';
+
+export function getUserInterests(): Promise<AxiosResponse<GetUserInterestsResponse>> {
+  const config: AxiosRequestConfig = {
+    url: '/user/interest',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  };
+
+  return axios.request<GetUserInterestsResponse>(config);
+}
---
+++ src/utils/typings/settings_interfaces/interests_interface/getInterestCategories.interface.ts
@@ -0,0 +1,10 @@
+interface Interests{
+    category:string; 
+    _id:string; 
+    index:number;
+}
+
+export interface GetInterestCategoriesResponse{
+    success:boolean;
+    interest_categories: Interests[];
+}
