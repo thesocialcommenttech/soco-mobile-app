238ae279e97084521131401801590e2b6b195ea3
Kapadiatathya <kapadiatathya@gmail.com>
2022-08-04 15:10:04 +0530

    Addition of otherscreens in navigation in stack

--- src/navigation/portfolioStack.tsx
+++ src/navigation/portfolioStack.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import Portfolio from '../screens/portfolio/portfolio';
+import ArtWorkDetail from '../screens/portfolio/artWorkDetail';
+import SkillVideoDetail from '../screens/portfolio/skillVideoDetail';
+import PresentationDetail from '../screens/portfolio/presentationDetail';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const PortfolioStack = () => {
-  return <Portfolio />;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Portfolio"
+        component={Portfolio}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Workdetail"
+        component={ArtWorkDetail}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SkillVideo"
+        component={SkillVideoDetail}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Presentation"
+        component={PresentationDetail}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default PortfolioStack;
