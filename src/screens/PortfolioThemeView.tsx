c150c69be87388840595a75ac9b9a644dc4684c8
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2022-09-20 13:07:11 +0530

    Portfolio theme view now showing issue fix

--- src/screens/PortfolioThemeView.tsx
+++ src/screens/PortfolioThemeView.tsx
@@ -1,24 +1,29 @@
 import { StyleSheet } from 'react-native';
-import React, { useState } from 'react';
+import React from 'react';
 import { WebView } from 'react-native-webview';
 import { useRoute } from '@react-navigation/native';
 import { PortfolioSubScreen_ScreenProps } from '../types/navigation/portfolio';
 import Loading from '../components/theme/Loading';
 
 export default function PortfolioThemeView() {
-  const [loading, setLoading] = useState(true);
   const route =
     useRoute<PortfolioSubScreen_ScreenProps<'PortfolioTheme'>['route']>();
 
   return (
-    <>
-      {loading && <Loading style={{ alignItems: 'center' }} />}
-      <WebView
-        source={{ uri: route.params.uri }}
-        onLoadStart={() => setLoading(true)}
-        onLoadEnd={() => setLoading(false)}
-      />
-    </>
+    <WebView
+      source={{ uri: route.params.uri }}
+      startInLoadingState
+      renderLoading={() => (
+        <Loading
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0
+          }}
+        />
+      )}
+    />
   );
 }
 
