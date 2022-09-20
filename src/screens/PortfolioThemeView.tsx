3a3a5a97bad2bdd1c012e39b88cf270d12de934e
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2022-09-20 15:18:47 +0530

    Added Back button functionality

--- src/screens/PortfolioThemeView.tsx
+++ src/screens/PortfolioThemeView.tsx
@@ -1,16 +1,34 @@
-import { StyleSheet } from 'react-native';
-import React from 'react';
+import { BackHandler, StyleSheet } from 'react-native';
+import React, { useEffect, useRef } from 'react';
 import { WebView } from 'react-native-webview';
 import { useRoute } from '@react-navigation/native';
 import { PortfolioSubScreen_ScreenProps } from '../types/navigation/portfolio';
 import Loading from '../components/theme/Loading';
 
 export default function PortfolioThemeView() {
+  const webViewRef = useRef<WebView>();
   const route =
     useRoute<PortfolioSubScreen_ScreenProps<'PortfolioTheme'>['route']>();
 
+  const onBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+    return () =>
+      BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+  }, []);
+
   return (
     <WebView
+      ref={webViewRef}
       source={{ uri: route.params.uri }}
       startInLoadingState
       renderLoading={() => (
