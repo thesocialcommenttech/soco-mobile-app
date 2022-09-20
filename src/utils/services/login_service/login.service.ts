32cfd8b48bac71189965d6b49b06d9919be7b626
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2022-09-20 13:53:53 +0530

    TopBar profile image is not loading issue fix

--- src/utils/services/login_service/login.service.ts
+++ src/utils/services/login_service/login.service.ts
@@ -1,14 +1,10 @@
-import axios from '../../axios';
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import {
   LoginRequestData,
   LoginResponseData
 } from '../../typings/login_interface/login.interface';
 
-export function login({
-  email,
-  password
-}: LoginRequestData): Promise<AxiosResponse<LoginResponseData>> {
+export function login({ email, password }: LoginRequestData) {
   const config: AxiosRequestConfig = {
     url: '/user/login',
     method: 'POST',
@@ -17,27 +13,5 @@ export function login({
     },
     data: { email, password }
   };
-  return axios
-    .request<LoginResponseData>(config)
-    .then(res => {
-      return res;
-    })
-    .catch(error => {
-      if (error.response) {
-        // Request made and server responded
-        // console.log(error.response.data);
-        // console.log(error.response.status);
-        // console.log(error.response.headers);
-        return error.response;
-      } else if (error.request) {
-        // The request was made but no response was received
-        // console.log(error.request);
-        return error.request;
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        // console.log('Error', error.message);
-        return error.message;
-      }
-      // return error;
-    });
+  return axios.request<LoginResponseData>(config);
 }
