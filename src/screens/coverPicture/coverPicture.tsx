a0702fe6415df92c3d41c2710be52e20a4285821
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2022-11-09 21:23:48 +0530

    fix: profile/cover image not showing when added in registration flow

--- src/screens/coverPicture/coverPicture.tsx
+++ src/screens/coverPicture/coverPicture.tsx
@@ -16,11 +16,20 @@ import {
   OptionalStackHeader
 } from '~/src/components/headers/OptionalStackHeader';
 import Button from '~/src/components/theme/Button';
+import { useDispatch } from 'react-redux';
+import { ThunkDispatch } from 'redux-thunk';
+import {
+  AuthActionTypes,
+  updateUserProfileImageGlobalState
+} from '~/src/store/actions/auth';
+import { IRootReducer } from '~/src/store/reducers';
 
 function CoverPictureScreen() {
   const navigation = useNavigation();
   const [image, setImage] = useState<ImagePicker.Asset>();
   const [loading, setLoading] = useState(false);
+  const dispatch =
+    useDispatch<ThunkDispatch<IRootReducer, any, AuthActionTypes>>();
 
   async function onImageSelect() {
     try {
@@ -44,6 +53,7 @@ function CoverPictureScreen() {
       });
 
       if (result.data.success) {
+        dispatch(updateUserProfileImageGlobalState(result.data.DPImageURL));
         navigation.navigate('BioScreen');
       }
 
